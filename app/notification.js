5b4e3994cfd935ead032bef728ba1c76e360aa27
uGroupid <ugroupid>
2018-01-19 15:13:17 +0700

    edit notifacations controller

--- app/notification.js
+++ app/notification.js
@@ -1,7 +1,8 @@
 function loadIt() {
     jQuery('#notifications_response').empty();
-    $.get(url_global+"cms/notification", function( notifications_response ) {
-        $( "#notifications_response" ).html( notifications_response );
-      });
-  }
+    jQuery.get(url_global+"cms/notification", function( notifications_response ) {
+        jQuery( "#notifications_response" ).html( notifications_response );
+    });
+}
+loadIt();
 setInterval(loadIt, 20000);
