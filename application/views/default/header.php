d09a31475978334f138a38189bd225de77c9e08f
uGroupid <ugroupid>
2018-01-19 14:38:49 +0700

    edit notifacations controller

--- application/views/default/header.php
+++ application/views/default/header.php
@@ -53,44 +53,9 @@
 		
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-		<li class="dropdown notifications-menu">
-           
-            <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                      page and may cause design problems
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-users text-red"></i> 5 new members joined
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-user text-red"></i> You changed your username
-                    </a>
-                  </li>
-                </ul>
-              </li>
-             
-            </ul>
-          </li>
+	  	<li class="dropdown notifications-menu" id="notifications_response">
+
+      </li>
           <li class="dropdown user user-menu">
            <?php 
                 $user_data = $this->session->userdata('data_users');
