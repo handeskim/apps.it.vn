d2707bcb2d8ff29d65f1db2656391b8c4834d4d7
uGroupid <ugroupid>
2018-01-19 14:22:52 +0700

    notifacations controller

--- application/modules/cms/controllers/notification.php
+++ application/modules/cms/controllers/notification.php
@@ -7,30 +7,70 @@ class Notification extends MY_Controller{
 		$this->login = $this->session->userdata('auth_sign');
 		if($this->login){
 			$this->user_data = $this->session->userdata('data_users');
-			$this->permisson = $this->user_data['authorities'];
+			$this->authorities = $this->user_data['authorities'];
 			$this->staff = $this->user_data['id'];
 		}else{
 			redirect(base_url('sign'));
 		}
 		
 	}
+	private function Notification(){
+		$staff =  $this->staff ;
+		$authorities =  $this->authorities;
+		try{
+			$sql = "SELECT * FROM notification WHERE staff = 2 AND `status` = 1 AND authorities = 1";
+			return $this->GlobalMD->query_global($sql);
+		}catch (Exception $e) {
+			return false;
+		}
+	}
 	public function index(){
 		
-		echo "hello";
-		// $msg ='';
-		// $data = array(
-			// 'msg' => $msg,
-			// 'content' => $this->staffs(),
-			// 'user_data' => $this->user_data,
-			// 'title'=> 'Staff Management',
-			// 'title_main' => 'Staff Management',
-		// );
-		// $this->parser->parse('default/header',$data);
-		// $this->parser->parse('default/sidebar',$data);
-		// $this->parser->parse('default/main',$data);
-		// $this->parser->parse('default/layout/main_curd_account',$data);
-		// $this->parser->parse('default/footer',$data);
+		$this->temp_notifacation();
 	}
+	public function temp_notifacation(){
+		$data = $this->Notification();
+		$total_notifications = 0;
+		if($data){
+			$total_notifications = count($data);
+		}
+		$temp = '<ul class="dropdown-menu">
+              <li class="header">bạn có '.$total_notifications.' Thông báo</li>
+              <li>
+                <!-- inner menu: contains the actual data -->
+                <ul class="menu">
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
+                      page and may cause design problems
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-users text-red"></i> 5 new members joined
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-shopping-cart text-green"></i> 25 sales made
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i class="fa fa-user text-red"></i> You changed your username
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="footer"><a href="#">View all</a></li>
+            </ul>';
+		echo $temp;
+		}
 	
 }
 ?>
