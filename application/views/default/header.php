45e4822dfea843e16107acce7aa6e26a68fc5dc7
uGroupid <ugroupid>
2018-01-19 15:04:56 +0700

    edit notifacations controller

--- application/views/default/header.php
+++ application/views/default/header.php
@@ -123,13 +123,3 @@
       </div>
     </nav>
   </header>
-
-<script>
-function loadIt() {
-    jQuery('#notifications_response').empty();
-    $.get(url_global+"cms/notification", function( notifications_response ) {
-        $( "#notifications_response" ).html( notifications_response );
-      });
-  }
-setInterval(loadIt, 30000);
-</script>
