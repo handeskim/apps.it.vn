b724edaafb44a1c4b93bd3a31928e0b681debeb2
handesk <handeskim@users.noreply.github.com>
2018-01-20 19:02:07 +0700

    edit controller order news

--- application/modules/cms/views/main_oder_new.php
+++ application/modules/cms/views/main_oder_new.php
@@ -1,7 +1,33 @@
+<script src="<?php echo base_url();?>public/bower_components/ckeditor/ckeditor.js"> </script>
 <section class="content">
 <div class="row">
-  <div class="col-lg-12 col-xs-12">
-        <h2> Tạo đơn hàng</h2>
+  <div class="col-md-12">
+       <form role="form">
+       <div class="box box-info">
+            <div class="box-header">
+              <h3 class="box-title">CK Editor
+                <small>Advanced and full of features</small>
+              </h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body pad">
+              <form>
+                    <textarea id="editor1" name="editor1" rows="10" cols="80">
+                                            This is my textarea to be replaced with CKEditor.
+                    </textarea>
+              </form>
+            </div>
+          </div>
+       </form>
   </div>
 </div>
 </section>
+<script>
+  $(function () {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1')
+    //bootstrap WYSIHTML5 - text editor
+    $('.textarea').wysihtml5()
+  })
+</script>
